8ac74e3472c138b4f565676567f85cd834e3e477
HengkiAnggia <pengkyjd00@gmail.com>
2024-06-02 16:44:17 +0700

    update list project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,25 @@
+import ListProject from "@/components/pages/project/ListProject";
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-8">
+      <section className="w-full">
+        <h1 className="text-xl mb-10">my project</h1>
+        <p>
+          I&#39;m a frontend developer. Currently, I&#39;m still studying in my
+          final semester at Brawijaya University. I really like the technology
+          environment and have been studying this field for about 3 years. I
+          really enjoyed it🎉 i&#39;ts very exciting.
+        </p>
+      </section>
+
+      <ListProject />
+      <ListProject />
+      <ListProject />
+      <ListProject />
+    </div>
+  );
 };
 
 export default Project;
---
+++ components/pages/project/ListProject.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+import image from "@/public/images/team.jpg";
+
+const ListProject = () => {
+  return (
+    <div className="grid grid-cols-7 items-center gap-3 w-full border-b border-black dark:border-white pb-6 cursor-pointer">
+      <div className="col-span-2 overflow-hidden">
+        <Image
+          src={image}
+          width={200}
+          alt="image"
+          className="rounded-lg max-h-28 object-cover"
+        />
+      </div>
+
+      <div className="col-span-5">
+        <h1 className="text-3xl">Eventeer</h1>
+        <h1 className="text-sm text-gray-500">Aug 2023</h1>
+        <p className="text-sm line-clamp-3">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod animi
+          officiis maxime vero commodi non rerum alias, voluptas eum eligendi
+          facere quam. Similique reprehenderit reiciendis, quasi omnis odit enim
+          neque, optio consectetur velit dolores labore. Ratione harum obcaecati
+          non odit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ListProject;
