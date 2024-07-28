e6fc781fed34a2ab2d9b39c9d0bba3c1bb7e1d53
HengkiAnggia <pengkyjd00@gmail.com>
2024-07-28 21:13:32 +0700

    refactor: Update image paths and optimize images in ImageGallery and Movie components

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,13 @@ import Title from "@/components/Title";
 import ImageGallery from "@/components/pages/home/ImageGallery";
 import { compareDateWithNow } from "@/lib/utils";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import skills from "@/lib/data/skills.json";
 
-const skills = [
-  { title: "Next.js", experience: "2022-03-22", icon: "logos:nextjs-icon" },
-  { title: "React", experience: "2021-05-15", icon: "logos:react" },
-  { title: "JavaScript", experience: "2021-01-10", icon: "logos:javascript" },
-  {
-    title: "TypeScript",
-    experience: "2024-01-30",
-    icon: "logos:typescript-icon",
-  },
-  { title: "Node.js", experience: "2023-09-18", icon: "logos:nodejs-icon" },
-  {
-    title: "Express.js",
-    experience: "2024-03-05",
-    icon: "simple-icons:express",
-  },
-  { title: "MongoDB", experience: "2023-10-14", icon: "logos:mongodb-icon" },
-  { title: "Zod", experience: "2024-01-20", icon: "logos:zod" },
-  { title: "Flutter", experience: "2023-12-07", icon: "logos:flutter" },
-  { title: "Git", experience: "2022-03-01", icon: "logos:git-icon" },
-  {
-    title: "TailwindCSS",
-    experience: "2021-08-03",
-    icon: "logos:tailwindcss-icon",
-  },
-  {
-    title: "Bootstrap",
-    experience: "2022-04-03",
-    icon: "logos:bootstrap",
-  },
-  { title: "Redux", experience: "2022-04-25", icon: "logos:redux" },
-  { title: "Axios", experience: "2023-07-25", icon: "logos:axios" },
-  {
-    title: "Jest",
-    experience: "2023-12-10",
-    icon: "vscode-icons:file-type-jest",
-  },
-];
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 export default function Home() {
   return (
@@ -57,28 +26,33 @@ export default function Home() {
 
         <section className="w-full">
           <h1 className="mb-4 text-xl max-md:mb-4">My coding skills 🎉</h1>
-          <div className="w-full grid grid-cols-3 gap-2">
+          <div className="w-full flex items-center justify-center flex-wrap gap-2">
             {skills.map((skill) => {
               return (
-                <div
-                  key={skill.title}
-                  className="flex flex-col items-center gap-2 px-4 py-5"
-                >
-                  <div className="flex gap-4 items-center">
-                    <Icon
-                      icon={skill.icon}
-                      className={
-                        skill.title == "Axios" ? `text-xl` : `text-4xl`
-                      }
-                    />
-                    {skill.title == "Axios" ? (
-                      ""
-                    ) : (
+                <>
+                  <HoverCard>
+                    <HoverCardTrigger className="no-underline cursor-pointer">
+                      <div
+                        key={skill.title}
+                        className="flex flex-col items-center gap-2 px-4 py-5"
+                      >
+                        <div className="flex gap-4 items-center">
+                          <Icon
+                            icon={skill.icon}
+                            className={
+                              skill.title == "Axios" ? `text-xl` : `text-4xl`
+                            }
+                          />
+                    
+                        </div>
+                      </div>
+                    </HoverCardTrigger>
+                    <HoverCardContent>
                       <h1 className="text-xl">{skill.title}</h1>
-                    )}
-                  </div>
-                  <p>{compareDateWithNow(skill.experience)} experience</p>
-                </div>
+                      <p>{compareDateWithNow(skill.experience)} experience</p>
+                    </HoverCardContent>
+                  </HoverCard>
+                </>
               );
             })}
           </div>
